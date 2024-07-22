e892a1d811da86368c266954710f48fbbbb0a4f3
Luiggis Laptop <github@luiggi33.de>
2024-07-23 00:04:48 +0200

    attempt users data source

---
+++ internal/provider/users_data_source.go
@@ -0,0 +1,122 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Luiggi33/pterodactyl-client-go"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ datasource.DataSource              = &usersDataSource{}
+	_ datasource.DataSourceWithConfigure = &usersDataSource{}
+)
+
+// NewUsersDataSource is a helper function to simplify the provider implementation.
+func NewUsersDataSource() datasource.DataSource {
+	return &usersDataSource{}
+}
+
+// usersDataSource is the data source implementation.
+type usersDataSource struct {
+	client *pterodactyl.Client
+}
+
+// usersDataSourceModel maps the data source schema data.
+type usersDataSourceModel struct {
+	Users []User `tfsdk:"users"`
+}
+
+// Users schema data.
+type User struct {
+	ID         int       `json:"id"`
+	ExternalID string    `json:"external_id"`
+	UUID       string    `json:"uuid"`
+	Username   string    `json:"username"`
+	Email      string    `json:"email"`
+	FirstName  string    `json:"first_name"`
+	LastName   string    `json:"last_name"`
+	Language   string    `json:"language"`
+	RootAdmin  bool      `json:"root_admin"`
+	Is2FA      bool      `json:"2fa"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+// Metadata returns the data source type name.
+func (d *usersDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_users"
+}
+
+// Schema defines the schema for the data source.
+func (d *usersDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{},
+	}
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (d *usersDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state usersDataSourceModel
+
+	users, err := d.client.GetUsers()
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Read Pterodactyl Users",
+			err.Error(),
+		)
+		return
+	}
+
+	// Map response body to model
+	for _, user := range users {
+		userState := User{
+			ID:         user.ID,
+			ExternalID: user.ExternalID,
+			UUID:       user.UUID,
+			Username:   user.Username,
+			Email:      user.Email,
+			FirstName:  user.FirstName,
+			LastName:   user.LastName,
+			Language:   user.Language,
+			RootAdmin:  user.RootAdmin,
+			Is2FA:      user.Is2FA,
+			CreatedAt:  user.CreatedAt,
+			UpdatedAt:  user.UpdatedAt,
+		}
+
+		state.Users = append(state.Users, userState)
+	}
+
+	// Set state
+	diags := resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+// Configure adds the provider configured client to the data source.
+func (d *usersDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Add a nil check when handling ProviderData because Terraform
+	// sets that data after it calls the ConfigureProvider RPC.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*pterodactyl.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *hashicups.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
+}
